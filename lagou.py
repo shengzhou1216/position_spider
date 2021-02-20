9ebe0b3ad19e6160e04c9eb925dd279c311f15de
zhousheng <zhousheng1216@sina.com>
2021-02-20 19:11:21 +0800

    feat: add wordcloud

--- lagou.py
+++ lagou.py
@@ -37,7 +37,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 baseUrl = 'https://www.lagou.com/jobs/positionAjax.json?needAddtionalResult=false'
 
 cookie = 'JSESSIONID=ABAAAECAAEBABII236533B72AD04387C58BBAEF92E48242; WEBTJ-ID=20210217134011-177ae80c802768-0b9786c38f28ee-4353760-1327104-177ae80c803836; RECOMMEND_TIP=true; index_location_city=%E5%85%A8%E5%9B%BD; sajssdk_2015_cross_new_user=1; sensorsdata2015session=%7B%7D; user_trace_token=20210217134024-93d7808b-ed6c-423c-91a6-3cec94d15e25; __lg_stoken__=59ac34ae6495c7995a689a9594aba0e1c3a8fc28ae5fd8b1cfa652e416b4c6017a05db450e4a62a6fda84cf0558ba4c7c31f1aeb8e809ddad9ae6148434a87cf688bbe5268c9; X_MIDDLE_TOKEN=8140b4a55171a02ac6df637b78a5e073; _ga=GA1.2.1994363303.1613540446; _gid=GA1.2.846640884.1613540446; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613194655,1613540446; LGUID=20210217134052-98a3f5ba-a88b-4224-b3f0-44f302723294; TG-TRACK-CODE=search_code; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22177ae80fbe562f-053cfdc37d7cad-4353760-1327104-177ae80fbe6557%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E8%87%AA%E7%84%B6%E6%90%9C%E7%B4%A2%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC%22%2C%22%24latest_referrer%22%3A%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24os%22%3A%22Windows%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%2283.0.4103.116%22%7D%2C%22%24device_id%22%3A%22177ae80fbe562f-053cfdc37d7cad-4353760-1327104-177ae80fbe6557%22%7D; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613545674; LGRID=20210217154341-63de2e9c-45d6-4143-9858-16b293f64196; X_HTTP_TOKEN=d1ad9a02ec4599c997605531616a97fc359620de80; SEARCH_ID=fa69bb704b3048d4a77c641af2a1ecce'
-detail_cookie = 'SESSIONID=ABAAAECAAEBABIIC9C5A9035A7ECD679762399A58FE53FF; WEBTJ-ID=02192021%2C105533-177b836c77c9b5-01282c63719253-1e2e1b0b-1648656-177b836c77d1278; RECOMMEND_TIP=true; index_location_city=%E5%85%A8%E5%9B%BD; sajssdk_2015_cross_new_user=1; sensorsdata2015session=%7B%7D; _ga=GA1.2.1138532040.1613703338; _gid=GA1.2.1361933110.1613703338; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613703339; user_trace_token=20210219105539-1935529d-b94d-498e-95c5-bcfc7137596f; LGSID=20210219105539-5532dfcb-ea6b-48b8-aa9c-842bf4d5ca75; LGUID=20210219105539-145370ca-0fa4-4478-af62-ab1687935f5a; __lg_stoken__=64f0ec0b3d36153bd482389b49cdd0ada7b0012a22b664fa9e30d2655005dcf803cf5462b480f88ce32e2c3a62a9f75f2ee7e8cf44cc8d62bf33400d0cea2df2ad7267f48ae2; X_MIDDLE_TOKEN=158eec6a338fdef4dc11990b47ed3ca6; SEARCH_ID=72b6b75a58f049b1b1c8578cdcf08c98; TG-TRACK-CODE=search_code; X_HTTP_TOKEN=eb64c8059176738a4834073161f2d27565626be941; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22177b836d83330a-0bccbaae45d1b7-1e2e1b0b-1648656-177b836d8341e8%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24os%22%3A%22UNIX%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%2287.0.4280.88%22%7D%2C%22%24device_id%22%3A%22177b836d83330a-0bccbaae45d1b7-1e2e1b0b-1648656-177b836d8341e8%22%7D; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613704387; LGRID=20210219111313-c99b4d9c-1521-4496-9140-147abaf902c1'
+detail_cookie = 'JSESSIONID=ABAAAECAAEBABIIC9C5A9035A7ECD679762399A58FE53FF; WEBTJ-ID=02192021%2C105533-177b836c77c9b5-01282c63719253-1e2e1b0b-1648656-177b836c77d1278; RECOMMEND_TIP=true; index_location_city=%E5%85%A8%E5%9B%BD; sensorsdata2015session=%7B%7D; _ga=GA1.2.1138532040.1613703338; Hm_lvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613703339; user_trace_token=20210219105539-1935529d-b94d-498e-95c5-bcfc7137596f; LGUID=20210219105539-145370ca-0fa4-4478-af62-ab1687935f5a; __lg_stoken__=64f0ec0b3d36153bd482389b49cdd0ada7b0012a22b664fa9e30d2655005dcf803cf5462b480f88ce32e2c3a62a9f75f2ee7e8cf44cc8d62bf33400d0cea2df2ad7267f48ae2; X_MIDDLE_TOKEN=158eec6a338fdef4dc11990b47ed3ca6; TG-TRACK-CODE=search_code; _gid=GA1.2.1562694723.1613793617; PRE_UTM=; PRE_HOST=; PRE_LAND=https%3A%2F%2Fwww.lagou.com%2Fjobs%2F7977723.html; LGSID=20210220120019-523d6559-e25b-4c09-83d4-68dbbfbef1ff; PRE_SITE=; sensorsdata2015jssdkcross=%7B%22distinct_id%22%3A%22177b836d83330a-0bccbaae45d1b7-1e2e1b0b-1648656-177b836d8341e8%22%2C%22first_id%22%3A%22%22%2C%22props%22%3A%7B%22%24latest_traffic_source_type%22%3A%22%E7%9B%B4%E6%8E%A5%E6%B5%81%E9%87%8F%22%2C%22%24latest_search_keyword%22%3A%22%E6%9C%AA%E5%8F%96%E5%88%B0%E5%80%BC_%E7%9B%B4%E6%8E%A5%E6%89%93%E5%BC%80%22%2C%22%24latest_referrer%22%3A%22%22%2C%22%24os%22%3A%22UNIX%22%2C%22%24browser%22%3A%22Chrome%22%2C%22%24browser_version%22%3A%2287.0.4280.88%22%7D%2C%22%24device_id%22%3A%22177b836d83330a-0bccbaae45d1b7-1e2e1b0b-1648656-177b836d8341e8%22%7D; _gat=1; Hm_lpvt_4233e74dff0ae5bd0a3d81c6ccf756e6=1613793791; LGRID=20210220120313-29080882-f07b-409e-a8a0-61bb809e8310; X_HTTP_TOKEN=eb64c8059176738a8183973161f2d27565626be941'
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:79.0) Gecko/20100101 Firefox/79.0',
     'Accept': 'application/json, text/javascript, */*; q=0.01',
@@ -54,10 +54,10 @@ detail_headers = {
     'Cache-Control': 'no-cache',
     'Cookie': detail_cookie,
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'referer': 'https://www.lagou.com/jobs/list_%E5%89%8D%E7%AB%AF%E5%B7%A5%E7%A8%8B%E5%B8%88/p-city_0?&cl=false&fromSearch=true&labelWords=&suginput='
+    'referer': 'https://www.lagou.com/utrack/trackMid.html?f=https%3A%2F%2Fwww.lagou.com%2Fjobs%2F7977723.html&t=1613793639&_ti=3'
 }
 proxies = {
-    'http': 'socks5://127.0.0.1:1080'
+    # 'http': 'socks5://127.0.0.1:1080'
 }
 
 
@@ -133,11 +133,19 @@ def process_positions():
 def get_detail_of_positon(positionId):
     position_url = 'https://www.lagou.com/jobs/%s.html' % positionId
     logging.info('positon_url:%s',position_url)
-    r = requests.get(position_url, headers=detail_headers, verify=False,proxies=proxies)
+    r = requests.get(position_url, headers=detail_headers, verify=False)
+    print(r.text)
     soup = BeautifulSoup(r.text,'lxml')
     return soup.select('.job-detail')[0].get_text()
 
 
+def store_to_file():
+    details = db.lagou.find({'detail':{'$exists': True}},{'detail': 1,'_id':False})
+    with open('lagou.txt','w',encoding='utf8') as f:
+        for d in details:
+            f.write(d['detail'])        
+
 if __name__ == '__main__':
     # start()
-    do_process_positions()
+    # do_process_positions()
+    store_to_file()
--- word_cloud.py
+++ word_cloud.py
@@ -3,8 +3,42 @@ import os
 
 from os import path
 from wordcloud import WordCloud
+import jieba
 
 text = open('lagou.txt').read()
 
-wordcloud = WordCloud().generate(text)
+# seg_list = jieba.cut(text)
+
+stopwords = []
+
+with open('stopwords.txt',encoding='utf-8') as f:
+    for l in f.readlines():
+        # l = l.strip('\n').replace('/','').replace('，','').replace(',','').replace('.','').replace('、','').replace(';','').replace('；','').replace('。','').replace(':','').replace('：','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','').replace('10','').replace('0','')
+        stopwords.append(l.strip('\n'))
+
+with open('custom_stopwords.txt',encoding='utf-8') as f:
+    for l in f.readlines():
+        # l = l.strip('\n').replace('/','').replace('，','').replace(',','').replace('.','').replace('、','').replace(';','').replace('；','').replace('。','').replace(':','').replace('：','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','').replace('10','').replace('0','')
+        stopwords.append(l.strip('\n'))
+
+chtext = ''
+with open('lagou.txt',encoding='utf-8') as f:
+    for l in f.readlines():
+        # l = l.strip('\n').replace('/','').replace('，','').replace(',','').replace('.','').replace('、','').replace(';','').replace('；','').replace('。','').replace(':','').replace('：','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','').replace('10','').replace('0','')
+        chtext += ' '.join([word for word in jieba.cut(l) if word not in stopwords])
+
+# Generate a word cloud image
+# lower max_font_size
+font = 'SimHei.ttf'
+wordcloud = WordCloud(max_font_size=40,font_path=font).generate(chtext)
+
+# Display the generated image:
+# the matplotlib way:
+import matplotlib.pyplot as plt
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.figure()
+
+wordcloud.to_file('lagou.jpg')
+
 
